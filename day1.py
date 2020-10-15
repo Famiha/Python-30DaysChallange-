618c709b1c38818e55a25c520405fcd251d628c4
Famiha Riasa <Famiha.raisa972@gmail.com>
2020-10-14 20:38:23 -0700

    Python Day1 & Day2 Added

---
+++ day1.py
@@ -0,0 +1,41 @@
+# ## Integer ##c
+# x  = 10
+# print("Integer Number", x)
+# print(type(x))
+
+# ## FLOAT ##
+
+# x = 2.5
+# print("Float Number",x)
+# print(type(x))
+
+# ## BOOLEAN ##
+# y = True
+# print(y)
+# print(type(y))
+
+# y = False
+# print(y)
+# print(type(y))
+
+## Char ##
+x = 10
+print(x+int("10"))
+
+## Arthimetic Operators ##
+
+## Simple and Basic Calculator ##
+
+# num1 = int(input("Enter your first number: "))
+# num2 = int(input("Enter your second number: "))
+
+# add = num1+num2
+# sub = num1-num2
+# mul = num1*num2
+# div = num1/num2
+# rem = num1%num2
+# exp = 3**2
+# floor = 10//4
+# print(f"Addition: {add}\n Sub: {sub}\n Mul: {mul}\n Div: {div}\n Rem: {rem}\n Exp{exp}\n Floor {floor}")
+
+# print('Hello this is cool "quote" ')
